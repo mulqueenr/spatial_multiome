5dabf3906aae66f7da2d4d7d2adf08206a545817
Ryan Mulqueen <mulqueenr@gmail.com>
2025-03-24 15:31:11 -0500

    Update spatial_processing_arc.groovy

--- nextflow_running/spatial_processing_arc.groovy
+++ nextflow_running/spatial_processing_arc.groovy
@@ -289,7 +289,7 @@ workflow {
 // DNA PROJECT SINGLE CELL DNA COMPLEXITY
     CELLRANGER_COUNT.out.multiome_outdir \
 	| DNA_SPLIT_BAM \
-	| buffer(1)
+	| buffer(1) \
 	| DNA_PROJECT_COMPLEXITY
 
 // DNA RUN COPYKIT
